1cdfca970c7a97fc6d3706ac9f3e9b28d17118ba
Ron Almog <ron8132@gmail.com>
2023-01-23 20:59:33 -0500

    nav links and animation

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,5 +1,6 @@
 import { Bars3Icon } from "@heroicons/react/24/solid";
 import Link from "next/link";
+import NavLinks from "./NavLinks";
 
 function Header() {
   return (
@@ -22,6 +23,10 @@ function Header() {
           </button>
         </div>
       </div>
+
+      <NavLinks />
+
+      {/* search box */}
     </header>
   );
 }
