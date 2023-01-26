7ae216eeb599353c8c1a39d7d9dc348de4e1baab
Ron Almog <ron8132@gmail.com>
2023-01-25 21:10:01 -0500

    add line-clamp npm, article visuals

--- app/Article.tsx
+++ app/Article.tsx
@@ -3,7 +3,30 @@ type Props = {
 };
 
 function Article({ article }: Props) {
-  return <div>Article</div>;
+  return (
+    <article className="bg-slate-100 dark:bg-slate-800 flex flex-col rounded-lg shadow-sm hover:bg-slate-200 transition-all duration-200 ease-out">
+      {article && (
+        <img
+          src={article.image}
+          alt={article.title}
+          className="h-56 w-full object-cover rounded-t-lg shadow-md"
+        />
+      )}
+      <div className="flex-1 flex flex-col">
+        <div className="flex-1 flex flex-col p5">
+          <h2 className="font-bold font-serif">{article.title}</h2>
+          <section className="nt-2 flex-1">
+            <p className="text-xs line-clamp-3">{article.description}</p>
+          </section>
+          <footer>
+            <p>{article.source}</p>
+            <p>{article.published_at}</p>
+          </footer>
+        </div>
+        read more
+      </div>
+    </article>
+  );
 }
 
 export default Article;
